b5578c9bda6775e58c39180c6d2e8d147d1094c5
Henri Losoi <mathharbour1@gmail.com>
2011-10-07 14:16:53 +0300

    added python -units

--- Research/Indifference/Tests/mathVals.py
+++ Research/Indifference/Tests/mathVals.py
@@ -1,20 +1,35 @@
 import random, math, numpy
 import unittest
+import sys
 
+sys.path.append('/home/olohuone/Coding/io8oi/Research/Indifference/')
 
+import lib.readDateVals as rv
 
 
 class TestMathFunctions(unittest.TestCase):
 
     def setUp(self):
+	self.seq = range(10)
 	self.returns = [[random.random()*x for x in range(10)] for _ in range(10)]
 	self.sds = [[random.random()*x**2 for x in range(10)] for _ in range(10)]
 	self.corrMatrix = numpy.corrcoef(self.returns)
 	
+    def testRandomChoices(self):
+	randomSd = random.choice(self.sds)
+	randomRe = random.choice(self.returns)
+	aves = rv.aves(self.returns)
+	randomAve = random.choice(aves)
 
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
+	self.assertTrue(randomSd in self.sds)
+	self.assertTrue(randomAve in aves)
+	self.assertTrue(randomRe in self.returns)
+	
+    def checkSize(self):
+	size = size(self.returns)
+	self.assertTrue(10 == size)
+	
+	# Check SDs and returns work correctly
 
 
 # Provides commandLine interface
