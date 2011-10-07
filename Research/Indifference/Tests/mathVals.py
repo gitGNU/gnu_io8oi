c56c92d8481854f672b243396b5b7c3d509049e2
Henri Losoi <mathharbour1@gmail.com>
2011-10-07 23:59:33 +0300

    added test for SDs to be positive

--- Research/Indifference/Tests/mathVals.py
+++ Research/Indifference/Tests/mathVals.py
@@ -1,6 +1,7 @@
 import random, math, numpy
 import unittest
 import sys
+import itertools
 
 sys.path.append('/home/olohuone/Coding/io8oi/Research/Indifference/')
 
@@ -11,6 +12,7 @@ class TestMathFunctions(unittest.TestCase):
 
     def setUp(self):
 	self.seq = range(10)
+	self.data = [[random.uniform(0,100) for _ in range(10)] for _ in range(10)]
 	self.returns = [[random.random()*x for x in range(10)] for _ in range(10)]
 	self.sds = [[random.random()*x**2 for x in range(10)] for _ in range(10)]
 	self.corrMatrix = numpy.corrcoef(self.returns)
@@ -25,6 +27,11 @@ class TestMathFunctions(unittest.TestCase):
 	self.assertTrue(randomAve in aves)
 	self.assertTrue(randomRe in self.returns)
 	
+    def testOrder(self):
+	mySds = rv.sds(self.sds, 365)
+	#SDs must be positive (not zero and not negative)
+	self.assertTrue(sum([sd <= 0 for sd in mySds])==0)
+
     def checkSize(self):
 	size = size(self.returns)
 	self.assertTrue(10 == size)
