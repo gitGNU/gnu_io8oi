867ae2700fba2fae12b90316c6a1b8eed3146d86
Henri Losoi <henrilosoi@gmail.com>
2012-01-05 07:15:48 +0200

    organizing this mess, broke the dxmn landing search -- making it clean...

---
+++ Gadgets/bin/generateIndex.r
@@ -0,0 +1,11 @@
+#
+# Author:	MathHarbour.com, Henri Losoi
+# License:	ISC
+#
+# Description:	generate the index based on a few keywords
+#		without hard-coding things like now in
+#		layouts.
+#
+#
+
+
