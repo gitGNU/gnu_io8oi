2a737e1f42801e68e34e980235c7af2ad4180aec
Henri Losoi <henrilosoi@gmail.com>
2012-01-12 17:37:23 +0200

    cleaned up this junk code, must kill it somehow

--- QR/bin/createBcQr.py
+++ QR/bin/createBcQr.py
@@ -1,13 +1,23 @@
 
 
-#TODO: generate QR image
-#QR image automatically?
+# TODO: reuse the createQR -function for this
+# TODO: THE LAYOUT to separate file.
+# TODO: SH -script
+#
+# Trial 0
+# $ python bin/createQr.py -f verySimpleQr
+#
+# TRIAL 1
+# $ python bin/createQr.py -f advancedQr -m 'I am 10801, yes!' -s 200
 
+amount=0.5
+imUrl='https://sites.google.com/site/mathharbour/search/qrCode_mh.gif'
+label="MathHarbour.com"
 
 print('Give me the BC code\n')
 code=raw_input()
 
-image='<div>\n <a href="bitcoin:'+code+'?amount=0.5&label=MathHarbour.com">\n <img src="https://sites.google.com/site/mathharbour/search/qrCode_mh.gif" ></a> \n <p>'+code+'</p>\n </div>'
+image='<div>\n<a href="bitcoin:'+code+'?amount='+str(amount)+'&label="'+label+'">\n <img src="'+imUrl+'" ></a> \n <p>'+code+'</p>\n </div>'
 
 textQr='<img src="http://ansrv.com/png?s=http://blockexplorer.com/q/getreceivedbyaddress/'+code+'&amp;c=000000&amp;b=FFFFFF&amp;size=5" />'
 
